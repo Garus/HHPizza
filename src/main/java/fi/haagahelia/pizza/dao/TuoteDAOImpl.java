93fae5cc3ca1428132e889ceca30e76b8d7f2eba
Koordinaattori <Koordinaattori@192.168.0.144>
2014-12-02 16:24:33 +0200

    Javadoc kommentointi - Viimeine Model tason luokka

--- src/main/java/fi/haagahelia/pizza/dao/TuoteDAOImpl.java
+++ src/main/java/fi/haagahelia/pizza/dao/TuoteDAOImpl.java
@@ -12,93 +12,123 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
+/**
+ * {@inheritDoc}
+ */
 @Repository
 public class TuoteDAOImpl implements TuoteDAO {
 
-	private static final Logger logger = Logger.getLogger(TuoteDAOImpl.class);
-	private SessionFactory sessionFactory;
+    private static final Logger logger = Logger.getLogger(TuoteDAOImpl.class);
+    private SessionFactory sessionFactory;
 
-	public void setSessionFactory(SessionFactory sf) {
-		this.sessionFactory = sf;
-	}
+    public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
 
-	@Override
-	public void addTuote(Tuote tuote) {
-		Session session = this.sessionFactory.getCurrentSession();
-		session.persist(tuote);
-		logger.info("Tuote tallennettu:: " + tuote);
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addTuote(Tuote tuote) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(tuote);
+        logger.info("Tuote tallennettu:: " + tuote);
 
-	}
+    }
 
-	@Override
-	public void updateTuote(Tuote tuote) {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateTuote(Tuote tuote) {
         Session session = this.sessionFactory.getCurrentSession();
         session.update(tuote);
         logger.info("Tuote päivitetty:: " + tuote);
 
-	}
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Tuote> haeKaikkiTuotteet() {
-		Session session = this.sessionFactory.getCurrentSession();
-		List<Tuote> tuotteetList = session.createQuery("from Tuote").list();
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Tuote> haeKaikkiTuotteet() {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Tuote> tuotteetList = session.createQuery("from Tuote").list();
 
-		for (Tuote tuote : tuotteetList) {
-			logger.info("Tuote:: " + tuote);
-		}
-		return tuotteetList;
-	}
+        for (Tuote tuote : tuotteetList) {
+            logger.info("Tuote:: " + tuote);
+        }
+        return tuotteetList;
+    }
 
-	@Override
-	public Tuote haeTuoteTunnuksella(int id) {
-		Tuote tuote = null;
-		Session session = this.sessionFactory.getCurrentSession();
-		logger.info("Trying to loaded pizza with ID " + id);
-		tuote = (Tuote) session.get(Tuote.class, new Integer(id));
-		logger.info("Pizza loaded successfully, Pizza = " + tuote);
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tuote haeTuoteTunnuksella(int id) {
+        Tuote tuote = null;
+        Session session = this.sessionFactory.getCurrentSession();
+        logger.info("Trying to loaded pizza with ID " + id);
+        tuote = (Tuote) session.get(Tuote.class, new Integer(id));
+        logger.info("Pizza loaded successfully, Pizza = " + tuote);
 
-		if (tuote == null) {
-			throw new TuoteNotFoundException("Ei löydetty tuotetta jonka id on: " + id);
-		}
+        if (tuote == null) {
+            throw new TuoteNotFoundException("Ei löydetty tuotetta jonka id on: " + id);
+        }
 
-		return tuote;
-	}
+        return tuote;
+    }
 
-	@Override
-	public void poistaTuote(Tuote p) {
-		Session session = this.sessionFactory.getCurrentSession();
-		session.delete(p);
-		logger.info("tuote :: " + p + " poistettu.");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void poistaTuote(Tuote p) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.delete(p);
+        logger.info("tuote :: " + p + " poistettu.");
 
-	}
+    }
 
-	@Override
-	public List<Kategoria> haeKaikkiKategoriat() {
-		Session session = this.sessionFactory.getCurrentSession();
-		List<Kategoria> kategoriat = session.createQuery("from Kategoria ").list();
-		logger.info("Kategoriat:: " + kategoriat);
-		return kategoriat;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Kategoria> haeKaikkiKategoriat() {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Kategoria> kategoriat = session.createQuery("from Kategoria ").list();
+        logger.info("Kategoriat:: " + kategoriat);
+        return kategoriat;
+    }
 
-	@Override
-	public List<Tuote> haeKategorianTuotteet(String kategoriaNimi) {
-		Session session = this.sessionFactory.getCurrentSession();
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Tuote> haeKategorianTuotteet(String kategoriaNimi) {
+        Session session = this.sessionFactory.getCurrentSession();
 		// select t.* from tuotteet t join kategoria_tuotteet kt on kt.tuotteet_id = t.id
-		// join kategoriat k on kt.kategoriat_kategoriaId = k.kategoriaId where k.kategoriaNimi = 'Pizzat';
-		List<Tuote> tuotteet = session.getNamedQuery("kategorianTuotteet").setString("kategoriaNimi", kategoriaNimi).list();
-		return tuotteet;
-	}
+        // join kategoriat k on kt.kategoriat_kategoriaId = k.kategoriaId where k.kategoriaNimi = 'Pizzat';
+        List<Tuote> tuotteet = session.getNamedQuery("kategorianTuotteet").setString("kategoriaNimi", kategoriaNimi).list();
+        return tuotteet;
+    }
 
-	@Override
-	public List<LisaAine> haeKaikkiLisaAineet() {
-		List<LisaAine> lisaAineet = this.sessionFactory.getCurrentSession().createQuery("from LisaAine  ").list();
-		return lisaAineet;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<LisaAine> haeKaikkiLisaAineet() {
+        List<LisaAine> lisaAineet = this.sessionFactory.getCurrentSession().createQuery("from LisaAine  ").list();
+        return lisaAineet;
+    }
 
-	@Override
-	public LisaAine haeLisaAineTunnuksella(int id) {
-		LisaAine aine = (LisaAine) this.sessionFactory.getCurrentSession().get(LisaAine.class, new Integer(id));
-		return aine;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public LisaAine haeLisaAineTunnuksella(int id) {
+        LisaAine aine = (LisaAine) this.sessionFactory.getCurrentSession().get(LisaAine.class, new Integer(id));
+        return aine;
+    }
 }
